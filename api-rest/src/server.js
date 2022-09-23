135ea1bf9bfce00b4e7d7e3095294ac4f54cbf38
Rafael Feitoza <rafaeljr@outlook.com>
2022-09-23 13:27:52 -0300

    feat: add `cors`

--- api-rest/src/server.js
+++ api-rest/src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { heroesRouter } = require('./routes');
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(heroesRouter);
 
