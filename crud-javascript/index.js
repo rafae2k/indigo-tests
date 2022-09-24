1e13c68f840e40acc7f2065b9c9aabd8a65db6ca
Rafael Feitoza <rafaeljr@outlook.com>
2022-09-23 21:29:47 -0300

    feat: implement edit and delete

--- crud-javascript/index.js
+++ crud-javascript/index.js
@@ -1,51 +1,130 @@
 const heroes = [];
 
-const form = document.getElementById('add-hero-form');
+let heroEditedIndex;
 
-const createTableRow = () => {
-  const list = document.getElementById('heroes-list');
-  const row = document.createElement('tr');
-  const id = document.createElement('td');
-  const name = document.createElement('td');
-  const description = document.createElement('td');
-
-  const lastHero = heroes[heroes.length - 1];
-
-  id.textContent = lastHero.id;
-  name.textContent = lastHero.name;
-  description.textContent = lastHero.description;
-
-  row.appendChild(id);
-  row.appendChild(name);
-  row.appendChild(description);
-  list.appendChild(row);
-};
-
-const addHero = (hero) => {
+// CRUD
+const createHero = (hero) => {
   heroes.push({
     id: heroes.length + 1,
     name: hero.name,
     description: hero.description,
   });
 
-  createTableRow();
+  updateTable();
 };
 
-form.addEventListener('submit', (e) => {
-  const { hero_name: name, hero_description: description } = form;
+const editHero = () => {
+  const name = document.getElementById('hero-edit-name');
+  const description = document.getElementById('hero-edit-description');
 
-  e.preventDefault();
-
-  // double negation to convert a possible falsy value to boolean
-  if (!!name && !!description) {
-    addHero({
-      name: form.hero_name.value,
-      description: form.hero_description.value,
-    });
-
-    form.hero_name.value = '';
-    form.hero_description.value = '';
-  } else {
-    alert('You cannot add an empty hero 😅');
+  if (!isFormValid({ name, description })) {
+    return alert('You cannot add an empty hero 😅');
   }
-});
+
+  heroes[heroEditedIndex] = {
+    id: heroes[heroEditedIndex].id,
+    name: name.value,
+    description: description.value,
+  };
+
+  name.value = '';
+  description.value = '';
+
+  updateTable();
+  closeModal();
+
+  heroEditedIndex = null;
+};
+
+const deleteHero = (index) => {
+  heroes.splice(index, 1);
+};
+
+const editOrDelete = (event) => {
+  if (event.target.type == 'button') {
+    const [action, index] = event.target.id.split('-');
+
+    if (action == 'edit') {
+      heroEditedIndex = index;
+      openModal();
+    } else {
+      deleteHero(index);
+      updateTable();
+    }
+  }
+};
+
+const openModal = () =>
+  document.getElementById('modal').classList.toggle('active');
+
+const closeModal = () => {
+  document.getElementById('modal').classList.remove('active');
+};
+
+updateTable = () => {
+  document.querySelector('#heroes-list>tbody').innerHTML = '';
+  heroes.forEach((hero, index) => {
+    createRow(hero, index);
+  });
+};
+
+const createRow = (hero, index) => {
+  const newRow = document.createElement('tr');
+  newRow.innerHTML = `
+      <td>${hero.id}</td>
+      <td>${hero.name}</td>
+      <td>${hero.description}</td>
+      <td class='buttons-wrapper'>
+          <button type="button" class="button edit" id="edit-${index}">Edit</button>
+          <button type="button" class="button delete" id="delete-${index}" >Delete</button>
+      </td>
+  `;
+
+  document.querySelector('#heroes-list>tbody').appendChild(newRow);
+};
+
+const clearForm = (form) => {
+  form.hero_name.value = '';
+  form.hero_description.value = '';
+};
+
+const isFormValid = (values) => {
+  const { name, description } = values;
+
+  if (!name.value) return alert('Hero name is required');
+
+  if (!description.value) return alert('Hero description is required');
+
+  if (!name.value && !description.value)
+    return alert('You cannot add an empty hero 😅');
+
+  return true;
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const { hero_name: name, hero_description: description } = e.target;
+
+  const isValid = isFormValid({ name, description });
+
+  if (isValid) {
+    createHero({ name: name.value, description: description.value });
+    clearForm(e.target);
+  }
+};
+
+// Event listeners
+document
+  .getElementById('add-hero-form')
+  .addEventListener('submit', handleSubmit);
+
+document
+  .querySelector('#heroes-list>tbody')
+  .addEventListener('click', editOrDelete);
+
+document.getElementById('save').addEventListener('click', editHero);
+
+document.getElementById('cancel').addEventListener('click', closeModal);
+
+// Create table on page load
+updateTable();
