499bd2d5a24e1fa6e7e0578412cca9b5512d9099
Rafael Feitoza <rafaeljr@outlook.com>
2022-09-25 11:47:15 -0300

    feat: rebase `react-query` branch

---
+++ crud-react/src/atoms/form.ts
@@ -0,0 +1,6 @@
+import { atom } from 'jotai';
+
+export const formAtom = atom({
+  name: '',
+  description: ''
+})
--- crud-react/src/services/api.ts
+++ crud-react/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { IHero } from '../types';
 
 const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3001'
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'
 })
 
 export const getHeroById = (id: number) => api.get<IHero>(`/heroes/${id}`)
