10ca5b8cbb0f4d9b34db049cba3d9a95af043f18
Rafael Feitoza <rafaeljr@outlook.com>
2022-09-25 11:10:18 -0300

    feat: add `PORT`

--- api-rest/src/server.js
+++ api-rest/src/server.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const { heroesRouter } = require('./routes');
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 app.use(heroesRouter);
 
-app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`app listening at ${PORT}`);
 });
